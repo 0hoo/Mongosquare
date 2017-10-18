e8162c60918af882c72c267e1eab661d2a7c12d4
Kim Younghoo <zerohoo.kim@gmail.com>
2017-10-19 02:08:08 +0900

    QueryFieldsTableViewDataSource를써서 리팩토링

--- Mongosquare/QueryWindowController.swift
+++ Mongosquare/QueryWindowController.swift
@@ -8,36 +8,81 @@
 
 import Cocoa
 
+typealias QueryField = (name: String, type: String)
+
+final class QueryFieldsTableViewDataSource: NSObject {
+    
+    var didSetSelectedFields: (([QueryField]) -> Void)?
+    var selectedRows = NSMutableOrderedSet()
+    var fields: [QueryField] = []
+    var selectedFields: [QueryField] = [] {
+        didSet {
+            didSetSelectedFields?(selectedFields)
+        }
+    }
+}
+
+extension QueryFieldsTableViewDataSource: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return fields.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let tableColumn = tableColumn else { return nil }
+        
+        let field = fields[row]
+        let view = tableView.make(withIdentifier: tableColumn.identifier, owner: self) as! NSTableCellView
+        if tableColumn.identifier == "DocumentColumnField" {
+            view.textField?.stringValue = field.name
+        } else if tableColumn.identifier == "DocumentColumnType" {
+            view.textField?.stringValue = field.type
+        }
+        return view
+    }
+}
+
+extension QueryFieldsTableViewDataSource: NSTableViewDelegate {
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        selectedRows.add(row)
+        return true
+    }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        guard let tableView = notification.object as? NSTableView else { return }
+        
+        let rowIndexes = tableView.selectedRowIndexes
+        let rowsToDelete = selectedRows.flatMap { $0 as? Int }.filter { rowIndexes.contains($0) == false }
+        for row in rowsToDelete {
+            selectedRows.remove(row)
+        }
+        
+        selectedFields = selectedRows.flatMap { $0 as? Int }.map { fields[$0] }
+    }
+}
+
 final class QueryWindowController: NSWindowController {
     override var windowNibName: String? { return "QueryWindowController" }
     
     @IBOutlet weak var fieldsTableView: NSTableView?
     @IBOutlet weak var fieldsSegmentedControl: NSSegmentedControl?
-    @IBOutlet weak var fieldsSearchField: NSSearchField?
     @IBOutlet weak var fieldsScanButton: NSButton?
+    @IBOutlet weak var fieldsSearchField: NSSearchField?
+    
+    @IBOutlet var dataSource: QueryFieldsTableViewDataSource?
     
     weak var collectionViewController: CollectionViewController?
     
     var didSave: (([String]) -> Void)?
-
-    typealias QueryField = (name: String, type: String)
     
-    var selectedRows = NSMutableOrderedSet()
-    
-    var fields: [QueryField] = []
-    var selectedFields: [QueryField] = [] {
-        didSet {
-            fieldsSearchField?.stringValue = "{ " + selectedFields.map { "\"\($0.name): 1\"" }.joined(separator: ", ") + " }"
-        }
-    }
     var projectingFields: [String]? = nil {
         didSet {
+            guard let dataSource = dataSource else { return }
             guard let projectingFields = projectingFields else { return }
             guard let fieldsTableView = fieldsTableView else { return }
-            let selectedRows: [Int] = projectingFields.flatMap { projectingField in fields.index(where: { projectingField == $0.name }) }
+            let selectedRows: [Int] = projectingFields.flatMap { projectingField in dataSource.fields.index(where: { projectingField == $0.name }) }
             if selectedRows.count > 0 {
                 let indices: IndexSet = IndexSet(selectedRows)
-                selectedRows.forEach { let _ = self.tableView(fieldsTableView, shouldSelectRow: $0) }
+                selectedRows.forEach { let _ = dataSource.tableView(fieldsTableView, shouldSelectRow: $0) }
                 fieldsTableView.selectRowIndexes(indices, byExtendingSelection: false)
             }
         }
@@ -46,10 +91,13 @@ final class QueryWindowController: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
         
-        fieldsSearchField?.delegate = self
-        
         let collectionName = collectionViewController?.collection?.fullName ?? ""
         window?.title = "Filter - \(collectionName)"
+
+        fieldsSearchField?.delegate = self
+        dataSource?.didSetSelectedFields = { [weak self] selectedFields in
+            self?.fieldsSearchField?.stringValue = "{ " + selectedFields.map { "\"\($0.name): 1\"" }.joined(separator: ", ") + " }"
+        }
         
         scan()
         projectingFields = collectionViewController?.projectingFields
@@ -58,16 +106,17 @@ final class QueryWindowController: NSWindowController {
     func scan() {
         guard let collectionViewController = collectionViewController else { return }
         
-        fields.removeAll()
+        dataSource?.fields.removeAll()
         let document = collectionViewController.documents[0]
         for key in document.keys {
             let valueType = document.type(at: key)?.description ?? ""
-            fields.append((name: key, type: valueType))
+            dataSource?.fields.append((name: key, type: valueType))
         }
         fieldsTableView?.reloadData()
     }
     
     @IBAction func save(_ sender: NSButton) {
+        guard let selectedFields = dataSource?.selectedFields else { return }
         close()
         didSave?(selectedFields.map { $0.name })
     }
@@ -77,53 +126,9 @@ final class QueryWindowController: NSWindowController {
     }
 }
 
-extension QueryWindowController: NSTableViewDataSource {
-    func numberOfRows(in tableView: NSTableView) -> Int {
-        if tableView == fieldsTableView {
-            return fields.count
-        }
-        return 0
-    }
-    
-    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        if tableView == fieldsTableView {
-            guard let tableColumn = tableColumn else { return nil }
-
-            let field = fields[row]
-            let view = tableView.make(withIdentifier: tableColumn.identifier, owner: self) as! NSTableCellView
-            if tableColumn.identifier == "DocumentColumnField" {
-                view.textField?.stringValue = field.name
-            } else if tableColumn.identifier == "DocumentColumnType" {
-                view.textField?.stringValue = field.type
-            }
-            return view
-        }
-
-        return nil
-    }
-}
-
-extension QueryWindowController: NSTableViewDelegate {
-    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
-        selectedRows.add(row)
-        return true
-    }
-
-    func tableViewSelectionDidChange(_ notification: Notification) {
-        guard let fieldsTableView = fieldsTableView else { return }
-        let rowIndexes = fieldsTableView.selectedRowIndexes
-        let rowsToDelete = selectedRows.flatMap { $0 as? Int }.filter { rowIndexes.contains($0) == false }
-        for row in rowsToDelete {
-            selectedRows.remove(row)
-        }
-
-        selectedFields = selectedRows.flatMap { $0 as? Int }.map { fields[$0] }
-    }
-}
-
 extension QueryWindowController: NSSearchFieldDelegate {
     func searchFieldDidEndSearching(_ sender: NSSearchField) {
-        selectedFields = []
+        dataSource?.selectedFields = []
         projectingFields = nil
         fieldsTableView?.deselectAll(self)
     }
