7acb402176e3156b882a65bed72619cebc7a9380
Sean Park <sean.2015@kakaocorp.com>
2018-06-08 17:00:17 +0900

    Update package

--- Package/Package.swift
+++ Package/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/idiel/MongoKitten.git", .revision("ac5aa7e"))
+    .package(url: "https://github.com/idiel/MongoKitten.git", .revision("8f92abe"))
         
     ],
     targets: [
