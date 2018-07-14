b858181aaf66ee0ec8bfe71c13be3a1f289ba5cc
Kim Younghoo <zerohoo.kim@gmail.com>
2018-07-14 23:40:31 +0900

    monaco editor 적용

--- Mongosquare/JsonViewController.swift
+++ Mongosquare/JsonViewController.swift
@@ -8,58 +8,80 @@
 
 import Cocoa
 import Cheetah
+import WebKit
+
+extension String {
+    func javascriptEscaped() -> String {
+        let str = self.replacingOccurrences(of: "\u{2028}", with: "\\u2028")
+                        .replacingOccurrences(of: "\u{2029}", with: "\\u2029")
+        // Because escaping JavaScript is a non-trivial task (https://github.com/johnezang/JSONKit/blob/master/JSONKit.m#L1423)
+        // we proceed to hax instead:
+        let data = try! JSONSerialization.data(withJSONObject: [str], options: [])
+        let encodedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)!
+        return encodedString.substring(with: NSMakeRange(1, encodedString.length - 2))
+    }
+}
 
 final class JsonViewController: NSViewController {
 
-    private let fragaria = MGSFragaria()
+    @IBOutlet weak var webView: WebView!
     
     weak var collectionViewController: CollectionViewController?
     
     var document: SquareDocument? {
         didSet {
             guard let document = document else {
-                fragaria.setString("")
                 return
             }
             var documentString = "\(document)"
             documentString = documentString.replacingOccurrences(of: "{", with: "{\n\t")
             documentString = documentString.replacingOccurrences(of: ",", with: ",\n\t")
             documentString = documentString.replacingOccurrences(of: "}", with: "\n}")
-            fragaria.setString(documentString)
+            documentString =  "\(documentString)".javascriptEscaped()
+            let call = "editor.setValue(\(documentString))"
+            let _ = webView?.stringByEvaluatingJavaScript(from: call)
+            let _ = webView?.stringByEvaluatingJavaScript(from: "editor.focus()")
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        fragaria.setObject(self, forKey: MGSFODelegate)
-        fragaria.embed(in: view)
-        fragaria.setObject("JavaScript", forKey: MGSFOSyntaxDefinitionName)
-    }
-    
-    func save() {
-        do {
-            let updated = SquareDocument(document: Document(try JSONObject(from: fragaria.string())))
-            if updated["_id"] == nil {
-                let result = try collectionViewController?.collection?.insert(updated)
-                print("insert?: \(String(describing: result))")
-            } else {
-                let result = collectionViewController?.collection?.update(updated)
-                print("update?: \(String(describing: result))")
+        if let path = Bundle.main.path(forResource: "editor", ofType: "html") {
+            do {
+                let content = try String(contentsOfFile: path)
+                let resourcePath = Bundle.main.resourcePath!
+                let baseURL = URL(fileURLWithPath: resourcePath + "/monaco/")
+                webView?.mainFrame.loadHTMLString(content, baseURL: baseURL)
+            } catch {
+                print(error)
             }
-            collectionViewController?.reload()
-        } catch {
-            print(error)
         }
     }
     
+    func save() {
+//        do {
+//            let updated = SquareDocument(document: Document(try JSONObject(from: fragaria.string())))
+//            if updated["_id"] == nil {
+//                let result = try collectionViewController?.collection?.insert(updated)
+//                print("insert?: \(String(describing: result))")
+//            } else {
+//                let result = collectionViewController?.collection?.update(updated)
+//                print("update?: \(String(describing: result))")
+//            }
+//            collectionViewController?.reload()
+//        } catch {
+//            print(error)
+//        }
+    }
+    
     func newDocument() {
-        document = SquareDocument(document: Document())
-        fragaria.setString("")
+//        document = SquareDocument(document: Document())
+//        fragaria.setString("")
     }
     
     func documentDeleted() {
-        document = nil
-        fragaria.setString("")
+//        document = nil
+//        fragaria.setString("")
     }
 }
