41d6542390e126348e5fa17d9812d4776b97da5f
Kim Younghoo <zerohoo.kim@gmail.com>
2018-07-16 19:05:11 +0900

    monaco editor focus problem

--- Mongosquare/JsonViewController.swift
+++ Mongosquare/JsonViewController.swift
@@ -11,14 +11,16 @@ import Cheetah
 import WebKit
 
 extension String {
-    func javascriptEscaped() -> String {
+    func javascriptEscaped() -> String? {
         let str = self.replacingOccurrences(of: "\u{2028}", with: "\\u2028")
                         .replacingOccurrences(of: "\u{2029}", with: "\\u2029")
         // Because escaping JavaScript is a non-trivial task (https://github.com/johnezang/JSONKit/blob/master/JSONKit.m#L1423)
         // we proceed to hax instead:
-        let data = try! JSONSerialization.data(withJSONObject: [str], options: [])
-        let encodedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)!
-        return encodedString.substring(with: NSMakeRange(1, encodedString.length - 2))
+        if let data = try? JSONSerialization.data(withJSONObject: [str], options: []),
+            let encodedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+            return encodedString.substring(with: NSMakeRange(1, encodedString.length - 2))
+        }
+        return nil
     }
 }
 
@@ -37,10 +39,9 @@ final class JsonViewController: NSViewController {
             documentString = documentString.replacingOccurrences(of: "{", with: "{\n\t")
             documentString = documentString.replacingOccurrences(of: ",", with: ",\n\t")
             documentString = documentString.replacingOccurrences(of: "}", with: "\n}")
-            documentString =  "\(documentString)".javascriptEscaped()
+            documentString =  "\(documentString)".javascriptEscaped() ?? ""
             let call = "editor.setValue(\(documentString))"
             let _ = webView?.stringByEvaluatingJavaScript(from: call)
-            let _ = webView?.stringByEvaluatingJavaScript(from: "editor.focus()")
         }
     }
     
